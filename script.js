198b3721432398a99af5008239ecadddf702f0bf
PetrilovaLia <natalia.petrilova@gmail.com>
2025-10-22 22:43:29 +0200

    Enhance timer and add random hex color generation
    Refactor timer and color generation functionality.

--- script.js
+++ script.js
@@ -1,40 +1,92 @@
-// výber html prvkov - id prvkov
-// js získa prístup, aby do nich vedel vložiť čísla
-const Days = document.getElementById('days');
-const Hours = document.getElementById('hours');
-const Minutes = document.getElementById('minutes');
-const Seconds = document.getElementById('seconds');
+// 1 KROK
+// Najskor si vytvorime premenne pre dve casti straanky:
+// Pomocou document.getElementById() na ne ziskame referencie.
 
-// nastavenie cieľového dátumu
-// newDate vytvorí objekt dátumu - konkrétny deň a čas
-// getTime() prevedie dátum na počet milisekúnd - Unix time 1970
-// vraj sa s tým lepšie počíta
-const targetDate = new Date('October 31 2025 00:00:00').getTime();
+// newColorButtonElement – tlacidlo
+const newColourBtnElement = document.getElementById('new-colour-button')
 
-// bude sa znovu a znovu vykonávať - každú sekundu
-function timer(){
-    const currentDate = new Date().getTime();
-    // koľko milisekúnd zostáva do cieľového dátumu
-    const distance = targetDate - currentDate;
+// currentColorElement – <span> s hex kodom
+const currentColourElement= document.getElementById('current-colour')
 
-    // milisekundy na dni, hodiny, minuty, sekundy
-    // math.floor zaokrúhli výsledok nadol, aby sme namali des. čísla
-    const days = Math.floor(distance / 1000 / 60 / 60 / 24);
-    // % 24 iba zvyšok po celých dňoch
-    const hours = Math.floor(distance / 1000 / 60 / 60) % 24;
-    // % 60 zobrazí 0 až 59 minút
-    const minutes = Math.floor(distance / 1000 / 60 ) % 60;
-    // 0 až 59 sekúnd
-    const seconds = Math.floor(distance / 1000 ) % 60;
+// 3 KROK
+// Dalej vytvorime pole hex hodnot: 0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F
+// na poradi nezalezi
+const hexValues = [
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+];
 
-    // console.log(days + ':' + hours + ':' + minutes + ':' + seconds)
+// 4 KROK
+// Potrebujeme funkciu, ktora vrati nahodny hex znak:
+// z pola hexValues vrati jeden nahodny znak
+function getRandomHexValue(){
+    const randomIndexPosition = Math.floor(
+        // Math.random() generuje nahodne cislo 0-1 (vylucena1)
+        // hexValues.length urcuje dlzku pola = 16 (znakov 0-9 A-F)
+        // dokopy vrati nahodne cislo 0-15.9999
+        Math.random() * hexValues.length
 
-    // premenná sa vloží do html prvku so zodpovedajúcim ID
-    // <h2 id="days"></h2> java napíše Days.innerHTML = 5; a na stránke vidíme číslo 5
-    Days.innerHTML = days;
-    Hours.innerHTML = hours;
-    Minutes.innerHTML = minutes;
-    Seconds.innerHTML = seconds;
+        // zabalime do Math.floor() ktora nam to zaokruhli 0-15
+        // presne ako pouzivame indexy v hexValues
+    )
+    // vysledok sa ulozi do premennej RandomHexVAlue
+
+    // vyberie prvok pola - randIndPosition = 10 vrati 'A'
+    const RandomHexValue = hexValues[randomIndexPosition];
+
+    // funkcia vrati random hodnotu
+    return RandomHexValue;
 }
 
-setInterval(timer, 1000);
+// 5 KROK
+// Potom vytvorime funkciu, ktora z tychto znakov vytvori 
+// cely hex retazec (6 znakov):
+// stringLength - kolkokrat zavola getRandomHexValue()
+function getRandomHexString(stringLength){
+    // prazdny retazec na ukladanie znakov
+    let hexString = '';
+    // cyklus sa opakuje stringLength krat
+    for (let i = 0; i < stringLength; i++){
+        // getRandomHexValue vrati nahodny znak sa prida na koniec retazca
+        hexString += getRandomHexValue();
+    }
+        
+    // vrati vytvoreny retazec
+    return hexString;
+}
+
+// 2 KROK
+// Potom pridame event listener na tlacidlo a alert overi, ci to funguje
+newColourBtnElement.addEventListener('click', function() {
+    //alert('ok')
+    //alert(randomHexString);
+    
+    // 6 KROK Ked pouzivatel klikne na tlacidlo, zavolame tuto funkciu:
+    // generovanie hex retazca - definujem jeho dlzku (6)
+    const randomHexString = '#' + getRandomHexString(6);
+    
+    // zmena farby pozadia - jednoriadkove css
+    // element body nastavi farbu pozadia na zaklade vygenreovaneho retazca
+    document.body.style.setProperty(
+        'background-color',
+        randomHexString
+    );
+
+    // zobrazenie aky kod vygenerovalo #123abc
+    // cez nase id current-colour
+    currentColourElement.textContent = randomHexString;
+});
